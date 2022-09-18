e164aeaf1eec506b6eed1ae17ec485e4a4b9f109
dita25 <113040794+dita25@users.noreply.github.com>
2022-09-18 15:26:02 +0800

    Add files via upload

---
+++ TUGAS_3.py
@@ -0,0 +1,13 @@
+answer = input("greeting : ").lower().strip()
+
+if 'hello' in answer:
+    print ("$0")
+
+if "hello,Newman" in answer:
+    print ("$0")
+ 
+elif "H" == answer:
+    print ("$20")
+
+else:
+    print ("$100")
